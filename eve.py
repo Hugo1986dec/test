9ead4be463e59a63a217145c5ae9b48b2e672be1
Hugo Liu <hugo1986dec@gmail>
2021-08-30 23:02:30 +0800

    finished eve.py

--- eve.py
+++ eve.py
@@ -1,2 +1,19 @@
-name = input('請輸入名字:')
-print('Hi loser',name)
+driving = input('請問你有開過車嗎？')
+if driving != '有' and driving != '沒有':
+   print('來亂的') 
+   raise SystemExit
+
+age = input('請輸入你的年齡')
+age = int(age)
+if driving == '有':
+   if age >= 18:
+      print('你可以合法上路')
+   else:
+      print('你違法了')   
+elif driving == '沒有':
+   if age >= 18:
+      print('嫩逼')
+   else:
+      print('乖') 
+
+   
